9a18b7d8d55406543a93f78343c0639fa0b4da70
RyanNguyen0203 <ryannguyen0203@gmail.com>
2017-07-05 12:21:25 +0700

    feat: basic functionality for testimonial slideshow

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Dropdown Menu
+
 var toggleMenu = document.querySelector('.toggle-menu');
 var toggleButton = document.querySelector('.toggle-button');
 var header = document.querySelector('header');
@@ -13,5 +15,53 @@ header.addEventListener('click', function(e){
 	if (toggleMenu.classList.contains('pull-menu-out')) {
 		toggleMenu.classList.remove('pull-menu-out');
 		toggleButton.classList.toggle('change-button');
+		e.stopPropagation();
 	}
+})
+
+// Slideshow (Testimonials)
+var slides = document.getElementsByClassName("slide");
+var len = slides.length; 
+var next = document.getElementById("next");
+var previous = document.getElementById("previous");
+
+for (var i=0; i<len; i++) {
+	slides[i].classList.toggle('slide-disappear');
+}
+
+// Turn on the first slide by default
+slides[0].classList.toggle('slide-disappear');
+var slide_shown = 0; 
+
+next.addEventListener('click', function(e){
+	// Hide the slide that is being shown 
+	slides[slide_shown].classList.toggle('slide-disappear');
+
+	// Show the next slide
+	if (slide_shown < len-1) { 
+		slides[slide_shown+1].classList.toggle('slide-disappear');
+		slide_shown ++;
+	} else {
+		// slides[len-1].classList.toggle('slide-disappear');
+		slides[0].classList.toggle('slide-disappear');
+		slide_shown = 0;
+	}
+	e.stopPropagation();
+})
+
+
+previous.addEventListener('click', function(e){
+	// Hide the slide that is being shown 
+	slides[slide_shown].classList.toggle('slide-disappear');
+
+	// Show the next slide
+	if (slide_shown > 0) { 
+		slides[slide_shown-1].classList.toggle('slide-disappear');
+		slide_shown --;
+	} else {
+		// slides[len-1].classList.toggle('slide-disappear');
+		slides[len-1].classList.toggle('slide-disappear');
+		slide_shown = len-1;
+	}
+	e.stopPropagation();
 })
