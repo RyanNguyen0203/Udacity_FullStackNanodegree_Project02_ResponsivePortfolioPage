d4c1b586cf60b01432967ab15cc744de42da2679
RyanNguyen0203 <ryannguyen0203@gmail.com>
2017-07-06 23:16:57 +0700

    style: format codes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
+	// Make semantic elements compatible with older versions of IE
+	document.createElement('article');
+	document.createElement('section');
+	document.createElement('aside');
+	document.createElement('hgroup');
+	document.createElement('nav');
+	document.createElement('header');
+	document.createElement('footer');
+	document.createElement('figure');
+	document.createElement('figcaption');
+
 	// Dropdown Menu
 	var toggleMenu = document.querySelector('.toggle-menu');
 	var toggleButton = document.querySelector('.toggle-button');
@@ -15,9 +26,9 @@ $(document).ready(function(){
 		e.stopPropagation();
 	})
 
-	// Close the dropdown menu when user clicks on header 
+	// Close the dropdown menu when user clicks on header
 	header.addEventListener('click', function(e){
-		// Close the dropdown menu 
+		// Close the dropdown menu
 		if (toggleMenu.classList.contains('pull-menu-out')) {
 			toggleMenu.classList.remove('pull-menu-out');
 			toggleButton.classList.toggle('change-button');
@@ -40,7 +51,7 @@ $(document).ready(function(){
 
 	// Slideshow (Testimonials)
 	var slides = document.getElementsByClassName("slide");
-	var len = slides.length; 
+	var len = slides.length;
 	var next = document.getElementById("next");
 	var previous = document.getElementById("previous");
 
@@ -50,14 +61,14 @@ $(document).ready(function(){
 
 	// Turn on the first slide by default
 	slides[0].classList.toggle('slide-disappear');
-	var slide_shown = 0; 
+	var slide_shown = 0;
 
 	next.addEventListener('click', function(e){
-		// Hide the slide that is being shown 
+		// Hide the slide that is being shown
 		slides[slide_shown].classList.toggle('slide-disappear');
 
 		// Show the next slide
-		if (slide_shown < len-1) { 
+		if (slide_shown < len-1) {
 			slides[slide_shown+1].classList.toggle('slide-disappear');
 			slide_shown ++;
 		} else {
@@ -70,11 +81,11 @@ $(document).ready(function(){
 
 
 	previous.addEventListener('click', function(e){
-		// Hide the slide that is being shown 
+		// Hide the slide that is being shown
 		slides[slide_shown].classList.toggle('slide-disappear');
 
 		// Show the next slide
-		if (slide_shown > 0) { 
+		if (slide_shown > 0) {
 			slides[slide_shown-1].classList.toggle('slide-disappear');
 			slide_shown --;
 		} else {
@@ -87,43 +98,43 @@ $(document).ready(function(){
 
 	// Scroll to section from navbar
 	$(".header-logo").click(function() {
-	    $('html, body').animate({
-	        scrollTop: $("body").offset().top
-	    }, 500);
+		$('html, body').animate({
+			scrollTop: $("body").offset().top
+		}, 500);
 	});
 
 	$(".go-to-my-story").click(function() {
-	    $('html, body').animate({
-	        scrollTop: $(".my-story").offset().top-150
-	    }, 500);
+		$('html, body').animate({
+			scrollTop: $(".my-story").offset().top-150
+		}, 500);
 	});
 
 	$(".go-to-featured-work").click(function() {
-	    $('html, body').animate({
-	        scrollTop: $(".featured-work").offset().top-300
-	    }, 500);
+		$('html, body').animate({
+			scrollTop: $(".featured-work").offset().top-300
+		}, 500);
 	});
 
 	$(".go-to-testimonials").click(function() {
-	    $('html, body').animate({
-	        scrollTop: $(".testimonials").offset().top-150
-	    }, 500);
+		$('html, body').animate({
+			scrollTop: $(".testimonials").offset().top-150
+		}, 500);
 	});
 
 	$(".go-to-connect").click(function() {
-	    $('html, body').animate({
-	        scrollTop: $(".connect").offset().top
-	    }, 500);
+		$('html, body').animate({
+			scrollTop: $(".connect").offset().top
+		}, 500);
 	});
 
 
-	// Make the header transparent when the user start scrolling 
+	// Make the header transparent when the user start scrolling
 	(function( $ ) {
 		$(function() {
-		    $( window ).scroll(function() {
-		    	/* Make the header bar transparent 
-		    	when the user starts scrolling */
-		    	$('.header-container').addClass('header-on-scroll');
+			$( window ).scroll(function() {
+				/* Make the header bar transparent
+				when the user starts scrolling */
+				$('.header-container').addClass('header-on-scroll');
 				clearTimeout( $.data( this, "scrollCheck" ) );
 				$.data( this, "scrollCheck", setTimeout(function() {
 					/* When scrolling stops, if header bar is at its
